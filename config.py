e61adb9de447a852ebbc618edb0f915894c0982c
irubey <isaac.rubey@gmail.com>
2024-12-28 16:35:02 -0700

    changed config

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
     
     # Database configuration
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    database_url = os.environ.get('DATABASE_URL')
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = database_url or 'sqlite:///climbing.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Session configuration
